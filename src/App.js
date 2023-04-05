37a7f1822b71bc2e6da0a2b72f134389d2613d68
Páli András <you@example.com>
2023-04-05 20:10:14 +0200

    search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  // local state
+  constructor() {
+    super(); // calls the constructor method on the parent class
+
+    // the state is always an object
+    this.state = {
+      coins: [],
+    };
+
+    // constructor gets called first
+    // console.log("1");
+  }
+  // will get run whenever a componenet gets rendered first time, it happens only one time
+  async componentDidMount() {
+    //gets called third bcs the componenet got mounted, and it changes the state which re-renders the component
+    // console.log("3");
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "ccaaca85d7msha40cbf1a90390b6p1fc03ejsn51ef863617a9",
+        "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+      },
+    };
+
+    const req = await fetch(
+      "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=5k-_VTxqtCEI&timePeriod=3h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
+      options
+    );
+    const data = await req.json();
+    // console.log(data);
+    this.setState(
+      () => {
+        return { coins: data.data.coins };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  }
+
   render() {
+    // gets called second with empty, initial state, THEN component gets MOUNTED
+    // render gets called twice bcs we update the state with setState in componentDidMount()
+    // console.log("2");
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          className="search-box"
+          type="text"
+          placeholder="seach coins"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        {this.state.coins.map((coin) => {
+          return (
+            <div key={coin.uuid}>
+              <h1>{coin.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
