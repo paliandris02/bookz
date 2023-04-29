cdb51aa29ae059259b2c69e7f6b38914046b146d
Páli András <you@example.com>
2023-04-29 20:51:50 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
-import "./App.css";
 
 class App extends Component {
   // local state
